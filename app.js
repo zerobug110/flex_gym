df1b7d5be4d339db46145e5832c5602b16cab092
zerobug110 <mahabojang110@gmail.com>
2020-10-31 16:28:49 +0000

    create an external link to images from js

--- app.js
+++ app.js
@@ -17,12 +17,10 @@ const name = document.querySelector('.review__name');
 const btnNext = document.querySelector('.btn-next');
 const btnPrev = document.querySelector('.btn-prev');
 
-
-
 const reviews = [
     {
         id: 1,
-        img: '../images/review-3.jpg',
+        img: 'https://media.istockphoto.com/photos/beautiful-african-american-female-model-picture-id910856488?k=6&m=910856488&s=612x612&w=0&h=r2YM9JLkSKBbs6gRrpUtK0ywxL7I9Vrt8cZELskqfs4=',
         heading: 'wow i lost 20 pounds in 1 month',
         desc: '  Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum illum reprehenderit earum, laborum voluptates perferendis, incidunt eum modi aperiam magnam vero impedit quibusdam, architecto quasi exercitationem repellendus magni ',
         name: 'muhammed touray'
@@ -30,7 +28,7 @@ const reviews = [
 
     {
         id: 1,
-        img: '../images/review-2.jpg',
+        img: 'https://media.gettyimages.com/photos/side-portrait-of-laughing-african-american-man-looking-up-picture-id1142003972?b=1&k=6&m=1142003972&s=612x612&w=0&h=tKbmg-ttIJ-93zCYFZspK071_wY0MQl3Fb2TnM6SA5U=',
         heading: 'this gym has all the equipments',
         desc: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat possimus suscipit cupiditate. Incidunt ullam, dolore illum reiciendis assumenda odit! Officiis vero neque voluptas dolore facilis, praesentium possimus molestiae porro asperiores.',
         name: 'fatou jawara'
@@ -39,7 +37,7 @@ const reviews = [
 
     {
         id: 1,
-        img: '../images/review-1.jpg',
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR9UZYu-s4xib8HAmaXCTEF6nfP2tIIPNZRwg&usqp=CAU',
         heading: 'finally i can train at night',
         desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla vel, saepe eaque ex vero nam quasi aperiam non autem! Eius eligendi tempore deleniti necessitatibus dolorum porro vitae ratione autem voluptas nesciunt nemo adipisci,',
         name: 'anna kane'
@@ -68,7 +66,7 @@ btnNext.addEventListener('click', ()=> {
     if(currentItem > reviews.length -1) {
         currentItem = 0;
     }
-}) 
+}); 
 
 btnPrev.addEventListener('click', ()=> {
     showPerson(currentItem);
